627cac14c94ea1b8e2d7d499cd7a0b046184b921
Nick Freear <nfreear@yahoo.co.uk>
2016-10-27 15:16:36 +0100

    Extend from a `defaults` JS variable; separate CSS stylesheet

---
+++ .env-example.js
@@ -0,0 +1,8 @@
+// Template for the Javascript (JSON) '.env.js' file.
+
+window.ENV = {
+  // Your Mapbox access token ('world' example only)
+  "ACCESS_TOKEN": "your.mapbox.public.access.token"
+}
+
+//End.
--- src/popup-geojson-map.js
+++ src/popup-geojson-map.js
@@ -7,24 +7,44 @@
 
   'use strict';
 
+  var defaults = {
+    latLng: [ 51.505, -0.09 ], // London, UK!
+    zoom: 3,
+    minZoom: 2,
+    maxZoom: 6,  //Was: 18,
+    opacity: 1,
+    mapId: 'mapid',
+    popupTemplate: '',  // Example: document.querySelector('#popup-template').innerText
+    checkProperty: 'audio_url',
+    geoJsonUrl: '../data/world-audio-geo-data.json', // alt: "?p=map/audio-geo-data.json", <!--EDIT-ME-->
+    tileUrl: 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
+    //'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+    subdomains: '',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    accessToken: '<%= ENV.ACCESS_TOKEN %>'
+  };
+
   var JSON = W.JSON  // Derive "globals".
     , request = W.superagent
     , L = W.L  // Leaflet
     , _ = W._  // Lodash
-    , CFG = W.MAP_CONFIG || {}
+    , CFG = _.extend(defaults, W.MAP_CONFIG)
     ;
+
+  W.console.debug('Map config:', CFG);
 
   var mymap = L.map(CFG.mapId).setView(CFG.latLng, CFG.zoom)
     , popup_template = _.template(CFG.popupTemplate)
+    , accessToken = _.template(CFG.accessToken)
     ;
 
   L.tileLayer(CFG.tileUrl, {
-  //L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 6,  //Was: 18,
-    minZoom: 2,
+    subdomains:  CFG.subdomains,
+    attribution: CFG.attribution,
+    maxZoom: CFG.maxZoom,
+    minZoom: CFG.minZoom,
     // Not needed! //id: 'your.mapbox.project.id',
-    accessToken: CFG.accessToken
+    accessToken: accessToken(W.ENV || { ENV: {} })
   }).addTo(mymap);
 
   request
@@ -33,7 +53,7 @@
 
     var geo_data = JSON.parse(response.text);
 
-    W.console.error(geo_data);
+    W.console.debug('GeoJSON:', geo_data);
 
     L.geoJson(geo_data, {
       onEachFeature: function (feature, layer) {
